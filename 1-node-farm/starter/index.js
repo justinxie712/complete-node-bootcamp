5340533033b6b4b90f8d74dc59854a2f18edfb4e
Justin Xie <justin.xie712@gmail.com>
2025-06-10 23:12:42 -0700

    simple api

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,8 +1,51 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+///////// FS //////////
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File written!");
+// sync way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written!");
+
+// // async way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+//   console.log("data", data);
+// });
+// console.log("Will read file!");
+
+///////// HTTP //////////
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if (pathName === "/") {
+    res.end("This is the homepage");
+  } else if (pathName === "/overview") {
+    res.end("This is the overview");
+  } else if (pathName === "/product") {
+    res.end("This is the product");
+  } else if (pathName === "/api") {
+    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+      res.writeHead(200, {
+        "Content-type": "application/json",
+      });
+      res.end(data);
+    });
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
