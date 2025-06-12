a20f8760ab5e4b8ab6121f75015e0b04b23e3d67
Justin Xie <justin.xie712@gmail.com>
2025-06-11 21:02:56 -0700

    finsih

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -2,23 +2,35 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
-///////// FS //////////
+/////////////////////////////////
+// FILES
 
-// sync way
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
-
 // const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync("./txt/output.txt", textOut);
-// console.log("File written!");
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File written!');
 
-// // async way
-// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-//   console.log("data", data);
+// Non-blocking, asynchronous way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR! 💥');
+
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('Your file has been written 😁');
+//       })
+//     });
+//   });
 // });
-// console.log("Will read file!");
+// console.log('Will read file!');
 
-///////// HTTP //////////
+/////////////////////////////////
+// SERVER
 const replaceTemplate = (temp, product) => {
   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
   output = output.replace(/{%IMAGE%}/g, product.image);
@@ -52,25 +64,37 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
-  if (pathName === "/" || pathName === "/overview") {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // Overview page
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, {
       "Content-type": "text/html",
     });
+
     const cardsHtml = dataObj
       .map((el) => replaceTemplate(tempCard, el))
       .join("");
     const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
     res.end(output);
-  } else if (pathName === "/product") {
-    res.end("This is the product");
-  } else if (pathName === "/api") {
-    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
-      res.writeHead(200, {
-        "Content-type": "application/json",
-      });
-      res.end(data);
+
+    // Product page
+  } else if (pathname === "/product") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
     });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    // API
+  } else if (pathname === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+
+    // Not found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
