e945d86b8d9c82d15f04ee382d4d7bce72344b2d
Justin Xie <justin.xie712@gmail.com>
2025-06-14 19:03:26 -0700

    asdf

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -6,12 +6,17 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 // 2. ROUTE HANDLERS
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋');
