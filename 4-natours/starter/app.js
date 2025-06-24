d03663d858987f7d0632e01de7ff05ca039d0f85
Justin Xie <justin.xie712@gmail.com>
2025-06-23 19:53:38 -0700

    error

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -11,8 +11,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
 
 // 2. ROUTE HANDLERS
 app.use(morgan('dev'));
@@ -20,13 +18,18 @@ app.use(morgan('dev'));
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
+  req.requestTime = new Date().toISOString();
   next();
 });
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
 });
 
 module.exports = app;
