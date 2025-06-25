56cf81c38c5d94e15794abff79d54c91bd38529c
Justin Xie <justin.xie712@gmail.com>
2025-06-24 21:01:51 -0700

    auth and sign up

--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -7,7 +7,11 @@ const {
   updateUser,
   deleteUser,
 } = require('../controllers/userController');
+const { signup } = require('../controllers/authController');
+
+router.post('/signup', signup);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
 module.exports = router;
